ca9c1ef6bf613b0070f8f313ac8e390924a3a7cb
绝世好男人 <422375723@qq.com>
2019-04-09 23:40:12 +0800

    擦

---
+++ core/orm/src/main/java/com/yhl/orm/componet/constant/FieldCondition.java
@@ -0,0 +1,9 @@
+package com.yhl.orm.componet.constant;
+
+import java.util.List;
+
+public class FieldCondition {
+    private String connection;
+    private List<FieldMsg> fieldMsgs;
+    private Integer order;
+}
