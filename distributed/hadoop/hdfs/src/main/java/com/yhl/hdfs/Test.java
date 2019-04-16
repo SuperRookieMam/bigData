1e136f8454ff3f7e9259536b7ddd11d45cd168af
SuperRookieMam <422375723@qq.com>
2019-04-16 16:11:34 +0800

    迭代

--- distributed/hadoop/hdfs/src/main/java/com/yhl/hdfs/Test.java
+++ distributed/hadoop/hdfs/src/main/java/com/yhl/hdfs/Test.java
@@ -1,6 +1,5 @@
 package com.yhl.hdfs;
 
-import com.yhl.hdfs.util.FsShellUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,10 +9,11 @@ public class Test {
     public static void main(String[] args) throws Exception {
       SpringApplication.run(Test.class, args);
       //HdfsFileUtil.upfile("C:\\Users\\Administrator\\Downloads\\1.rar","/test1","cover");
-      //HdfsFileUtil.downLoad("/test","C:\\Users\\Administrator\\Desktop\\test");
+      // HdfsFileUtil.downLoad("/kk/_SUCCESS","C:\\Users\\Administrator\\Desktop\\test");
+       // HdfsFileUtil.downLoad("/kk/part-r-00000","C:\\Users\\Administrator\\Desktop\\test");
      // System.out.println("亲测可用");
-        FsShellUtil fsShellUtil =new FsShellUtil();
-     fsShellUtil.run(" hadoop fs -ls /");
+     //   FsShellUtil fsShellUtil =new FsShellUtil();
+     //fsShellUtil.run(" hadoop fs -ls /");
     }
 
 
